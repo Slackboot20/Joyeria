679df4ab98fcdc32f4239bdac26775de4bba2b26
tife502 <125226019+tife502@users.noreply.github.com>
2024-10-05 21:19:48 -0500

    More Date

--- DateJoyas/Joyas-Date.js
+++ DateJoyas/Joyas-Date.js
@@ -7,5 +7,14 @@ export const jewelry = [
         precioInicial: '500.000',
         precioFinal: '600.000',
         provedor: 'Ecopetrol'
+    },
+    {
+        codProduct: '02',
+        description: 'Anillo de Plata entorchado con una piedra preciosa',
+        peso: '20 gramos',
+        material: 'Plata',
+        precioInicial: '100.000',
+        precioFinal: '150.000',
+        provedor: 'Ecopetrol'
     }
 ];
