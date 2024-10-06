0d41c998867c731e0cb757d514e9676aa1c970d7
Slackboot20 <camilo.ceballos@utp.edu.co>
2024-10-05 23:31:57 -0500

    joyas en ver joyas

--- DateJoyas/JoyasDate.js
+++ DateJoyas/JoyasDate.js
@@ -1,20 +1,22 @@
-export const jewelry = [
-    {
-        codProduct: '01',
-        description: 'Anillo de Oro entorchado con una piedra preciosa',
-        peso: '10 gramos',
-        material: 'Oro',
-        precioInicial: '500.000',
-        precioFinal: '600.000',
-        provedor: 'Ecopetrol'
-    },
-    {
-        codProduct: '02',
-        description: 'Anillo de Plata entorchado con una piedra preciosa',
-        peso: '20 gramos',
-        material: 'Plata',
-        precioInicial: '100.000',
-        precioFinal: '150.000',
-        provedor: 'Ecopetrol'
+export default function jewelry() {
+const jewel = [
+        {
+            codProduct: '01',
+            description: 'Anillo de Oro entorchado con una piedra preciosa',
+            peso: '10 gramos',
+            material: 'Oro',
+            precioInicial: '500.000',
+            precioFinal: '600.000',
+            provedor: 'Ecopetrol'
+        },
+        {
+            codProduct: '02',
+            description: 'Anillo de Plata entorchado con una piedra preciosa',
+            peso: '20 gramos',
+            material: 'Plata',
+            precioInicial: '100.000',
+            precioFinal: '150.000',
+            provedor: 'Ecopetrol'
+        }
+    ];
     }
-];
--- components/CardJewelry.js
+++ components/CardJewelry.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import { View,Text, StyleSheet } from 'react-native';
-import{ Card } from 'react-native-paper';
-
+import { View, Text, StyleSheet } from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
 
 const CardJewelry = () => {
-    return (
-        <View>
-            <Card>
-                <Card.Title  title="Jewelrys" subtitle="Jewelrys" />
-            </Card>
-        </View>
-    )
+  const jewel = [
+    {
+      codProduct: '01',
+      description: 'Anillo de Oro entorchado con una piedra preciosa',
+      peso: '10 gramos',
+      material: 'Oro',
+      precioInicial: '500.000',
+      precioFinal: '600.000',
+      provedor: 'Ecopetrol',
+    },
+    {
+      codProduct: '02',
+      description: 'Anillo de Plata entorchado con una piedra preciosa',
+      peso: '20 gramos',
+      material: 'Plata',
+      precioInicial: '100.000',
+      precioFinal: '150.000',
+      provedor: 'Ecopetrol',
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      {jewel.map((item, index) => (
+        <Card key={index} style={styles.card}>
+          <Card.Title title={`Joya ${item.codProduct}`} subtitle={item.description} />
+          <Card.Content>
+            <Paragraph>Peso: {item.peso}</Paragraph>
+            <Paragraph>Material: {item.material}</Paragraph>
+            <Paragraph>Precio Inicial: {item.precioInicial}</Paragraph>
+            <Paragraph>Precio Final: {item.precioFinal}</Paragraph>
+            <Paragraph>Proveedor: {item.provedor}</Paragraph>
+          </Card.Content>
+        </Card>
+      ))}
+    </View>
+  );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  card: {
+    marginBottom: 10,
+  },
+});
+
 export default CardJewelry;
-    
